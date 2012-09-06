2d3879914513dd5a1048e5862de0dd0cb67f41c6
Stephen Sylvia <ssylvia@esri.com>
2012-09-06 18:32:28 -0400

    image fader layout tweaks/change map extents

--- javascript/layout.js
+++ javascript/layout.js
@@ -123,6 +123,9 @@ var setLayers = function(sec){
         map.getLayer(findLayerName("PloverPoints")).show();
         map.getLayer(findLayerName("summer")).show();
         map.getLayer(findLayerName("winter")).hide();
+
+        map.setExtent(new esri.geometry.Extent({"xmin":-10079006.019723145,"ymin":3137303.833390451,"xmax":-6018671.077215662,"ymax":7114475.289123686,
+  "spatialReference":{"wkid":102100}}));
     }
     else if(sec === 2){
         map.getLayer(findLayerName("MigrationArrows")).setVisibleLayers([0]);
@@ -131,6 +134,9 @@ var setLayers = function(sec){
         map.getLayer(findLayerName("PloverPoints")).show();
         map.getLayer(findLayerName("summer")).hide();
         map.getLayer(findLayerName("winter")).hide();
+
+        map.setExtent(new esri.geometry.Extent({"xmin":-10504607.393214896,"ymin":2265310.214713391,"xmax":-6444272.450707414,"ymax":6242481.670446625,
+  "spatialReference":{"wkid":102100}}));
     }
     else if(sec === 3){
         map.getLayer(findLayerName("MigrationArrows")).hide();
@@ -138,6 +144,9 @@ var setLayers = function(sec){
         map.getLayer(findLayerName("PloverPoints")).show();
         map.getLayer(findLayerName("summer")).hide();
         map.getLayer(findLayerName("winter")).show();
+
+        map.setExtent(new esri.geometry.Extent({"xmin":-11297106.502475392,"ymin":1180515.909290458,"xmax":-7236771.559967909,"ymax":5157687.365023692,
+  "spatialReference":{"wkid":102100}}));
     }
     else if(sec === 4){
         map.getLayer(findLayerName("MigrationArrows")).setVisibleLayers([1]);
@@ -146,11 +155,17 @@ var setLayers = function(sec){
         map.getLayer(findLayerName("PloverPoints")).show();
         map.getLayer(findLayerName("summer")).hide();
         map.getLayer(findLayerName("winter")).hide();
+
+        map.setExtent(new esri.geometry.Extent({"xmin":-10504607.393214896,"ymin":2265310.214713391,"xmax":-6444272.450707414,"ymax":6242481.670446625,
+  "spatialReference":{"wkid":102100}}));
     }
     else{
         map.getLayer(findLayerName("MigrationArrows")).hide();
         map.getLayer(findLayerName("PloverPoints")).hide();
         map.getLayer(findLayerName("summer")).show();
         map.getLayer(findLayerName("winter")).show();
+
+        map.setExtent(new esri.geometry.Extent({"xmin":-13762659.286841381,"ymin":652183.1697834609,"xmax":-5641989.401826414,"ymax":8606526.081249928,
+  "spatialReference":{"wkid":102100}}));
     }
 };

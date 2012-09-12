1a83c3df5ecc809083390ed648d9c21dd2a5b803
Stephen Sylvia <ssylvia@esri.com>
2012-09-12 13:16:42 -0400

    wait for animation to be ready

--- javascript/layout.js
+++ javascript/layout.js
@@ -20,41 +20,20 @@ var setSection = function(sec){
     $("#sectionTitle").html(sectionData[sec].title);
     $("#sectionText").html(sectionData[sec].text);
 
-    $(".selectionBullet").remove();
-    $(".gallerySlide").remove();
+    $("#fader").children("img").remove();
     dojo.forEach(sectionData[sec].images,function(img){
-        $("#fader").append("<li class='gallerySlide'><img src='"+img.src+"' caption='"+img.copyright+"' class='galleryImg' alt=''></li>");
-        $("#imgSelector").append("<span class='selectionBullet'>&bull;</span>");
-    });
-    $("#imgSelector").css("left",($("#leftPane").width() - $("#imgSelector").width())/2);
-    $(".selectionBullet").eq(0).addClass("selectionBulletSelected");
-    if(sectionData[sec].images.length === 1){
-        $("#imgSelector").hide();
-    }
-    else{
-        $("#imgSelector").show();
-    }
-
-    $(".selectionBullet").click(function(){
-        $("#fader").imageFader("goTo",$(this).index());
+        $("#fader").append("<img src='"+img.src+"' caption='"+img.copyright+"' alt=''>");
     });
 
     if(firstLoad === false){
         firstLoad = true;
-        $("#fader").imageFader({
-            //captions : true,
-            //captionAttr : "caption",
-            animationEnd : function(obj){
-                $(".selectionBullet").removeClass("selectionBulletSelected");
-                $(".selectionBullet").eq(obj.index).addClass("selectionBulletSelected");
-            }
-        });
+        $("#fader").imageFader();
     }
     else{
         $("#fader").imageFader("update");
     }
 
-    setLayers(sec);
+    //setLayers(sec);
 
 };
 
